13fd1163e51e55dd85ba343407d4234b3d30eeb6
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-07-22 10:12:17 +0700

    feat: add dashboard shadcn block

---
+++ web-next/src/app/dashboard/page.tsx
@@ -0,0 +1,36 @@
+import data from "./data.json";
+import { AppSidebar } from "@/blocks/dashboard-01/app-sidebar";
+import { ChartAreaInteractive } from "@/blocks/dashboard-01/chart-area-interactive";
+import { DataTable } from "@/blocks/dashboard-01/data-table";
+import { SectionCards } from "@/blocks/dashboard-01/section-cards";
+import { SiteHeader } from "@/blocks/dashboard-01/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 14)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
