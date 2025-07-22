26be8cb9bcc06f65cea031345454350c582f2fa4
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-07-22 09:55:36 +0700

    feat: add inter font

--- web-next/src/app/layout.tsx
+++ web-next/src/app/layout.tsx
@@ -1,17 +1,9 @@
 import "./globals.css";
 import { AppProvider } from "@/components/app-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "React Playground - NextJS",
@@ -25,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${font.className} scroll-smooth antialiased`}>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
